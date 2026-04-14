79da935233e74b111d241ca83cac9afd9273b59a
izabelalsg-coder <izabelalsg@gmail.com>
2026-04-14 00:16:33 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nossa-familia-v4';
+const CACHE = 'nossa-familia-v5';
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/nossa-familia/', '/nossa-familia/index.html'])).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => {
